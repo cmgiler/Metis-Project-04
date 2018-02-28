a5a49052b3a171da7585e9f5b9ae138d6f37e2f8
Christopher Giler <chrisgiler@ip-172-31-84-139.ec2.internal>
2018-02-28 19:58:56 +0000

    Update Reuter's Dataset Pickle File (Migrate to Local)

--- FletcherLibrary/TextSummarization.py
+++ FletcherLibrary/TextSummarization.py
@@ -1,5 +1,7 @@
 from . import MongoQuery as MQ
 from . import TopicModel as TM
+import datetime as dt
+from collections import Counter
 import pickle
 
 def PredictTopic(content_list, name='reuters'):
@@ -15,13 +17,31 @@ def PredictTopic(content_list, name='reuters'):
 	topics = []
 	confidence = []
 
-	return count_vectorizer, counts
-
 	x = count_vectorizer.transform(content_list)
 
-	W = model.transform(x.toarray().T)[0]
+	W = model.transform(x.toarray())
 
 	topics = [category_names[w.argmax()] for w in W]
 	confidence = [w.max()/w.sum() for w in W]
 
 	return topics, confidence
+
+def FetchDataInDateRange(collection_name, start_date_str, end_date_str):
+    collection = MQ.GetMongoCollection('blog_data', collection_name)
+    start = dt.datetime.strptime(start_date_str, 
+                                 '%B %d, %Y').replace(hour=0, 
+                                                      minute=0, 
+                                                      second=0)
+    end = dt.datetime.strptime(end_date_str, 
+                               '%B %d, %Y').replace(hour=23, 
+                                                    minute=59, 
+                                                    second=59)
+    data = []
+    for d in collection.find({'datetime': {'$gt': start, '$lt': end}}):
+        if 'topic' in d.keys() and d['topic'] != '':            
+            data.append(d)
+    return data
+
+def GetCategoryCounts(collection_name, start_date_str, end_date_str):
+    data = FetchDataInDateRange(collection_name, start_date, end_date)
+    return Counter([d['topic'] for d in data])
